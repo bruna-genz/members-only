d781630234dd644c3b41927b56cd1d67e1c27c79
Bruna <brunagenz92@gmail.com>
2019-12-04 18:49:46 -0400

    create user resources and show action on users controller; define gravatar_for method on users helper

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'static_pages/jobs'
   get 'static_pages/videos'
   get 'static_pages/guides'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
