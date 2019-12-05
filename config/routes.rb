f07c7d2e0241678001dabe706bd6956f787919d7
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-12-05 15:10:32 -0500

    create sessions controller; add functionalities to sessions contruller; add styles to forms and debug errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get 'static_pages/snippets'
   get 'static_pages/newsletter'
   get 'static_pages/jobs'
   get 'static_pages/videos'
   get 'static_pages/guides'
+  get 'sessions/new'
+  post 'sessions/create'
+  delete 'sessions/destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
