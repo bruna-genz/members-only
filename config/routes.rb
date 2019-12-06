5dd5d8a97489285fca82e0a832be9e2779c33812
Bruna <brunagenz92@gmail.com>
2019-12-06 18:46:10 -0400

    generate scaffold to create posts controller, model and views

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   post 'sessions/create'
   delete 'sessions/destroy'
   resources :users
+  resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
