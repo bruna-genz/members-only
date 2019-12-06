8c16faf1fa71c207b00b95a26a53c14e5b33965c
Bruna <brunagenz92@gmail.com>
2019-12-06 15:59:31 -0400

    create before action to require the right user for edit and update actions; add functionality to friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def new
     @user = User.new
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
   end
 
-  def update 
-    @user = User.find(params[:id])
+  def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile Updated"
       redirect_to @user
@@ -42,9 +41,15 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "You must log in"
         redirect_to sessions_new_path
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def current_user?(user)
+    user == current_user
+  end
   
   def current_user
     if (user_id = session[:user_id])
@@ -37,5 +41,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
