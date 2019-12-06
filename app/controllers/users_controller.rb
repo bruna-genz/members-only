230544c854cd20e28e83cd147a6060c949b8d36e
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-12-06 14:41:33 -0500

    Update users; create settings form; require user be logged; Highlight errors in editing and log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -10,10 +12,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Members Only!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -22,4 +39,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "You must log in"
+        redirect_to sessions_new_path
+      end
+    end
+
 end
