f8e58deeb35d033c623edda8d67956167c1602ad
Bruna <brunagenz92@gmail.com>
2019-12-09 15:43:59 -0400

    delete Post scafford and generate post model and posts controller; define new post action and add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,23 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  
+  def new
+    @post = Post.new
+  end
 
+  def create
+  end
+
+  def index
+  end
+
+  private
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "You must log in"
+        redirect_to sessions_new_path
+      end
+    end
 end
